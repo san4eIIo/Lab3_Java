7eba1a88cd539920f82e5f40cd480250bf8db9a6
san4eIIo <99270874+san4eIIo@users.noreply.github.com>
2022-12-09 15:50:38 +0300

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Lab3 {
+    private static boolean isPallindrom(String[] a) {
+        int q = a.length;
+        for (int i = 0; i < q / 2; i++) {
+            if (!a[i].equals(a[q - i - 1])) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input str: ");
+        String[] x = scanner.nextLine().split("");
+        int len = x.length;
+        String a;
+        exitlabel:
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                a = "";
+                for (int k = j; k < len - i + j; k++) {
+                    a += (x[k]);
+                }
+                if (isPallindrom(a.split(""))) {
+                    System.out.print(a + " - Паліндром");
+                    break exitlabel;
+                }
+            }
+        }
+    }
+}
+//Прикла для перевірки: абабабаба бабабабаб оперрепо око
